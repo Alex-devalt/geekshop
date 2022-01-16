e6e0de47920c53e33e7d6a16ac959f9c2af36b0d
Alex-devalt <79976996+Alex-devalt@users.noreply.github.com>
2022-01-16 17:14:08 +0200

    fixtures/fill_db

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+JSON_PATH = 'mainapp/json'
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
